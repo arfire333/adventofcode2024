beb579a30a5c3ed2425ae2955d1d6394c494a287
Arfire <72059150+arfire333@users.noreply.github.com>
2024-12-08 16:49:20 -0500

    Day 08 Complete
      - Additionally started working fix for running on the web.
        Can run problem 1 on web but still need to implement a
        generic solution for getting data on the web.

---
+++ test/solutions/day08_solution_test.dart
@@ -0,0 +1,34 @@
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:adventofcode2024/data.dart';
+import 'package:adventofcode2024/solutions/day08_solution.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+String rawData = "............\n"
+    "........0...\n"
+    ".....0......\n"
+    ".......0....\n"
+    "....0.......\n"
+    "......A.....\n"
+    "............\n"
+    "............\n"
+    "........A...\n"
+    ".........A..\n"
+    "............\n"
+    "............\n";
+
+void main() async {
+  test('Part 1', () async {
+    Day08Solution solution = Day08Solution();
+    solution.parse(rawData);
+    solution.part1();
+    expect(solution.answer1, '14');
+  });
+
+  test('Part 2', () async {
+    Day08Solution solution = Day08Solution();
+    solution.parse(rawData);
+    solution.part2();
+    expect(solution.answer2, '34');
+  });
+}
